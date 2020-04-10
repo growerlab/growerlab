32e54ff2945572315783f6a61e1a2eb40971a8c6
moli <molix2@outlook.com>
2020-04-11 01:20:55 +0800

    fix:  router

--- router/build.sh
+++ router/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ ! -f ./router/main ]; then
-    cd ./router || exit 1
-    go build -o main ./main.go
-    cd - || exit 1
-fi
+# if [ ! -f ./router/main ]; then
+cd ./router || exit 1
+go build -o main ./main.go
+cd - || exit 1
+# fi
