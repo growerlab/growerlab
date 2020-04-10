df6c898f8048d75a3be27ab45571cdc9d9de9c47
moli <molix2@outlook.com>
2020-04-11 00:44:26 +0800

    fix: router name

--- router/build.sh
+++ router/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-if [ ! -f ./router/router ]
-then
-  cd ./router || exit 1
-  GOOS=linux go build -o ./router ./router.go
-  cd - || exit 1
+if [ ! -f ./router/main ]; then
+    cd ./router || exit 1
+    go build -o main ./main.go
+    cd - || exit 1
 fi
-
