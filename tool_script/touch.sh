f5d1102d90f3eec65c9039ddbdbda027540e5d6e
moli <molix2@outlook.com>
2020-09-07 00:36:59 +0800

    update 脚本

--- tool_script/touch.sh
+++ tool_script/touch.sh
@@ -11,13 +11,30 @@ FRONEND_WORKFLOW_ID=772221
 
 TARGET_WORKFLOW_ID=$BACKEND_WORKFLOW_ID
 
-ACTION_TARGET='backend'
+ACTION_TARGET_BACKEND='backend'
+ACTION_TARGET_FRONEND='fronend'
 
-if [ $1 = $ACTION_TARGET ]; then
+ACTION_TARGET_BRANCH=$2
+
+if [ $1 = $ACTION_TARGET_BACKEND ]; then
     TARGET_WORKFLOW_ID=$BACKEND_WORKFLOW_ID
 else
     TARGET_WORKFLOW_ID=$FRONEND_WORKFLOW_ID
 fi
+
+if [ -z $GITHUB_WORKFLOW_TOKEN ]; then
+    echo "err!"
+    echo "github token is required"
+    exit 1
+fi
+
+if [[ -z $ACTION_TARGET_BACKEND || -z $ACTION_TARGET_BRANCH ]]; then
+    echo "err!"
+    echo "eg: ./touch.sh backend master"
+    exit 1
+fi
+
+echo "touch $ACTION_TARGET_BACKEND $ACTION_TARGET_BRANCH ..."
 
 curl --location --request POST "https://api.github.com/repos/growerlab/growerlab/actions/workflows/$TARGET_WORKFLOW_ID/dispatches" \
     --header "Accept: application/vnd.github.v3+json" \
