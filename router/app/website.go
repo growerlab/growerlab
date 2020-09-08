8b016e1a423624daa879671a63372045a8bd0d20
moli <molix2@outlook.com>
2020-09-08 16:38:17 +0800

    优化：调整文件传输代码

--- router/app/website.go
+++ router/app/website.go
@@ -17,7 +17,6 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-	"time"
 )
 
 const (
@@ -67,37 +66,24 @@ func (w *WebSiteRouter) fileLocation(branch string, resp http.ResponseWriter, re
 	root := filepath.Join("/data", branch, "data/website")
 	file := filepath.Join(root, path)
 
-	modtime := time.Now()
-	var fp *os.File
-
 	switch path {
 	case "/":
 		path = DefaultIndex
 		file = filepath.Join(root, path)
-		stat, err := os.Stat(file)
-		if os.IsNotExist(err) {
-			resp.WriteHeader(http.StatusNotFound)
-			return
-		}
-		modtime = stat.ModTime()
-		fp, err = os.Open(file)
-		if err != nil {
-			resp.Write([]byte(err.Error()))
-			return
-		}
 	default:
 		file = filepath.Join(root, path)
-		stat, err := os.Stat(file)
-		if os.IsNotExist(err) {
-			path = DefaultIndex
-		}
-		file = filepath.Join(root, path)
-		modtime = stat.ModTime()
-		fp, err = os.Open(file)
-		if err != nil {
-			resp.Write([]byte(err.Error()))
-			return
-		}
+	}
+
+	stat, err := os.Stat(file)
+	if os.IsNotExist(err) {
+		resp.WriteHeader(http.StatusNotFound)
+		return
+	}
+	modtime := stat.ModTime()
+	fp, err := os.Open(file)
+	if err != nil {
+		resp.Write([]byte(err.Error()))
+		return
 	}
 
 	log.Printf("url: %s, client: %s, path: %s", req.URL.String(), req.RemoteAddr, file)
