8f44f612d52260cbe4177c8b60b70550b143995a
moli <molix2@outlook.com>
2022-10-21 09:01:12 +0000

    update: 调整调试路径

--- .dogo.sh
+++ .dogo.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set +x
 
+if [ ! -d ".repositories" ]; then
+    mkdir .repositories
+fi
+
 cd src || exit 1
 
 go run backend/main.go &
 
-if [ ! -f ./go-git-grpc/hooks/update ]; then
+if [ ! -f ./conf/hooks/update ]; then
     echo "building go-git-grpc 'update' hook..."
-    cd go-git-grpc && go build -o ./hooks/update ./script/hulk/main.go
+    cd go-git-grpc && go build -o ../conf/hooks/update ./script/hulk/main.go
     cd - || exit 1
 fi
 go run go-git-grpc/cli/main.go &
