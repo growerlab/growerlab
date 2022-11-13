8e162f62bc7b69cd584b4332f8d1afabd0cfa792
moli <molix2@outlook.com>
2022-11-14 00:51:28 +0800

    update：优化后台页面

--- src/frontend/src/pages/user/repos/Show.tsx
+++ src/frontend/src/pages/user/repos/Show.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { useTitle } from "react-use";
-import { useParams } from "react-router-dom";
 
 import { RepositoryDetail } from "../../../core/components/repository/RepositoryDetail";
 import { getTitle } from "../../../core/common/document";
-import { useGlobal } from "../../../core/global/init";
 import i18n from "../../../core/i18n/i18n";
 import Error404 from "../../common/404";
 import { useRepositoryPathGroup } from "../../../core/components/hook/repository";
