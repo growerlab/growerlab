09fbfbbbf697b7ba7455fb96280be8f4e9cab1da
moli <molix2@outlook.com>
2022-11-14 00:51:17 +0800

    update：优化后台页面

--- src/frontend/src/pages/user/repos/Show.tsx
+++ src/frontend/src/pages/user/repos/Show.tsx
@@ -7,28 +7,20 @@ import { getTitle } from "../../../core/common/document";
 import { useGlobal } from "../../../core/global/init";
 import i18n from "../../../core/i18n/i18n";
 import Error404 from "../../common/404";
+import { useRepositoryPathGroup } from "../../../core/components/hook/repository";
 
 export default function RepositoryShow() {
   useTitle(getTitle(i18n.t("repository.menu")));
-  const global = useGlobal();
-  const currentUser = global.currentUser;
 
-  let namespace = currentUser?.namespace;
+  const { namespace, repo, isInvalid } = useRepositoryPathGroup();
 
-  const { repoPath, namespacePath } = useParams();
-  useTitle(getTitle(repoPath));
-
-  if (namespacePath !== undefined) {
-    namespace = namespacePath;
-  }
-
-  if (repoPath === undefined || namespace === undefined) {
+  if (isInvalid) {
     return <Error404 />;
   }
 
   return (
     <div>
-      <RepositoryDetail namespace={namespace} repo={repoPath} />
+      <RepositoryDetail namespace={namespace} repo={repo} />
     </div>
   );
 }
